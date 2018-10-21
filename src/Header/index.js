2d14202446f46c7481edec59c745314c47fdb5b5
Dan <danhemerlein@gmail.com>
2018-10-21 13:01:42 -0400

    configure scss, add styleguide and utils

--- src/Header/index.js
+++ src/Header/index.js
@@ -1,16 +1,16 @@
-
 import React, { Component } from "react";
 
 export default class Header extends Component {
-  constructor(props) {
-    super(props)
-  }
-
   render() {
     return (
-      <div className="Header">
-        <h1 class="m0">Dan Hemerlein</h1>
-        <h2 class="m0">Creative Coder / Music Producer</h2>
+      <div className="flex">
+        <div class="col-6">
+          <h1 class="m0">Dan Hemerlein</h1>
+          <h2 class="m0">Creative Coder / Music Producer</h2>
+        </div>
+        <div class="flex justify-end col-6">
+          <h3>Keep In Touch</h3>
+        </div>
       </div>
     )
   }
