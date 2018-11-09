004eeb2b335f490d70a447ca79473ebaff08665b
Dan <danhemerlein@gmail.com>
2018-11-09 11:25:36 -0500

    set up contentful

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from "react-router-dom";
+import * as contentful from 'contentful';
+import keys from './config';
 
 import './styles/app.scss';
 
@@ -9,6 +11,19 @@ import Code from './components/Code';
 import MusicPage from './components/MusicPage';
 import Contact from './components/Contact';
 import Moodboard from './components/Moodboard';
+
+const client = contentful.createClient({
+  space: keys.spaceId,
+  accessToken: keys.accessToken
+})
+
+client.getEntries().then(entries => {
+  entries.items.forEach(entry => {
+    if (entry.fields) {
+      console.log(entry.fields)
+    }
+  })
+})
 
 class App extends Component {
   render() {
