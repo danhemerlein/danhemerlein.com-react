a0c0709c9f947fefbf2617534b8519ab4070ae80
Dan <danhemerlein@gmail.com>
2018-11-25 00:25:37 -0500

    set up sign up form comp

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -2,13 +2,14 @@ import React, { Component } from "react";
 
 import './Contact.scss'
 
+import SignUpForm from '../SignUpForm';
+
 export default class Contact extends Component {
   render() {
     return (
-      <div>
-        <div className="Contact">
-          <h6>This is the contact page</h6>
-        </div>
+      <div className="Contact">
+        <h6>This is the contact page</h6>
+        <SignUpForm />
       </div>
     )
   }
---
+++ src/components/SignUpForm/index.js
@@ -0,0 +1,112 @@
+import React, { Component } from "react";
+
+import './SignUpForm.scss'
+
+import ENV from '../../config/Enviornment';
+import MailchimpSubscribe from 'react-mailchimp-subscribe';
+
+const { MAILCHIMP_URL } = ENV;
+
+export default class SignUpForm extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      firstName: '',
+      emailAddress: '',
+      zipcode : '',
+      message: ''
+    }
+  }
+
+  handleNameChange = event => {
+    this.setState({
+      firstName: event.target.value,
+
+    });
+  };
+
+  handleEmailChange = event => {
+    this.setState({
+      emailAddress: event.target.value,
+    });
+  };
+
+  handleZipChange = event => {
+    this.setState({
+      zipcode: event.target.value
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        <div className="SignUpForm">
+          <MailchimpSubscribe
+            url={MAILCHIMP_URL}
+            render={({ subscribe, status }) => (
+              <div>
+                <form onSubmit={(event) => {
+                  event.preventDefault();
+                  subscribe({
+                    FIRSTNAME: this.state.firstName,
+                    EMAIL: this.state.emailAddress,
+                    ZIPCODE: this.state.zipcode
+                  })
+                }
+                }>
+                  <div className="">
+                    <label className="">
+                      <input
+                        className=""
+                        placeholder="First Name"
+                        value={this.state.firstName}
+                        onChange={this.handleNameChange}
+                      />
+                    </label>
+                    <label className="">
+                      <input
+                        className=""
+                        type="email"
+                        placeholder="hello@example.com"
+                        value={this.state.emailAddress}
+                        onChange={this.handleEmailChange}
+                      />
+                    </label>
+                    <label className="">
+                      <input
+                        className=""
+                        placeholder="Zipcode"
+                        value={this.state.zipcode}
+                        onChange={this.handleZipChange}
+                      />
+                    </label>
+                    <button className="" type="submit">
+                      Submit
+                    </button>
+                  </div>
+                  <p className="">{this.state.message}</p>
+                </form>
+                {status === 'sending' ? (
+                  this.setState({
+                    message: 'Loading ೭੧(❛▿❛✿)੭೨'
+                  })
+                ) : null}
+                {status === 'success' ? (
+                  this.setState({
+                    message: 'Thanks for subscribing (*＾v＾*)'
+                  })
+                ) : null}
+                {status === 'error' ? (
+                  this.setState({
+                    message: 'Oops, please try again (′ʘ⌄ʘ‵)'
+                  })
+                ) : null}
+              </div>
+            )}
+          />
+        </div>
+      </div>
+    )
+  }
+}
