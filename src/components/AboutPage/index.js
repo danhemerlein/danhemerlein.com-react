269fba8040625fab3a7f4412ed2f6e0157e667bc
Dan <danhemerlein@gmail.com>
2018-11-24 23:35:37 -0500

    add two about text blocks

--- src/components/AboutPage/index.js
+++ src/components/AboutPage/index.js
@@ -9,15 +9,22 @@ import './AboutPage.scss';
 class AboutPage extends Component {
   render() {
     const text = get(this, "props.text", []);
+    const half = Math.ceil(text.length / 2);
+    const leftSide = text.splice(0, half);
 
     return (
       <div>
         <div className="AboutPage my2">
           <div className="flex">
-            <div className="col-6">
+            <div className="col-4 body-serif center">
+              {shuffle(leftSide).map((item, key) => {
+                return <span className="AboutPage__item full-width">{item}</span>
+              })}
+            </div>
+            <div className="col-4">
               <Image src={this.props.image.fields.file.url} />
             </div>
-            <div className="col-6 body-serif center">
+            <div className="col-4 body-serif center">
               {shuffle(text).map((item, key) => {
                 return <span className="AboutPage__item full-width">{item}</span>
               })}
