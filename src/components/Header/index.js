aa8f1f2151922570350a15eaa02873ef5d648df2
Dan <danhemerlein@gmail.com>
2018-12-01 19:01:12 -0500

    add responsive styles to header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -3,22 +3,27 @@ import { Link } from "react-router-dom";
 
 import './Header.scss'
 
+import HamburgerIcon from '../icons/Hamburger';
+
 export default class Header extends Component {
   render() {
     return (
       <div className="Header">
         <div className="flex">
-          <div className="col-6">
+          <div className="Header__title col-6">
             <h1 className="heading-serif m0"><Link to="/">{this.props.title}</Link></h1>
-            <h2 className="heading-serif m0"><Link to="/code">{this.props.subTitle}</Link> / <Link to="/music">{this.props.subTitleTwo}</Link></h2>
+            <h2 className="xs-hide heading-serif m0"><Link to="/code">{this.props.subTitle}</Link> / <Link to="/music">{this.props.subTitleTwo}</Link></h2>
           </div>
-          <div className="flex justify-end col-6">
-            <nav role="navigation">
+          <div className="flex justify-end items-center col-6">
+            <nav className="xs-hide" role="navigation">
               <ul className="list-style-none p0">
                 <li className="body-serif inline-block"><Link to="/keep-in-touch">keep in touch</Link></li>
                 <li className="body-serif inline-block ml2"><Link to="/about">about</Link></li>
               </ul>
             </nav>
+            <div className="sm-hide md-hide lg-hide col-3">
+              <HamburgerIcon />
+            </div>
           </div>
         </div>
       </div>
