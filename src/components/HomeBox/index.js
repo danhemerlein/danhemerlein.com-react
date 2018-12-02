3fc694b06d4389118cdbfa2e460bab77c47ed8df
Dan <danhemerlein@gmail.com>
2018-12-01 22:32:52 -0500

    got hover to work on for homepage

--- src/components/HomeBox/index.js
+++ src/components/HomeBox/index.js
@@ -6,11 +6,11 @@ export default class HomeBox extends Component {
   render() {
     return (
       <div className="HomeBox flex justify-center items-center full-width">
-        <div className="Homebox__container block relative">
+        <div className="Homebox__c block relative">
           <div className="HomeBox__svg flex justify-center items-center full-width full-height">
             {this.props.icon}
           </div>
-          <div className="HomeBox__overlay absolute l0 r0 full-width full-height">
+          <div className="HomeBox__overlay bg-white flex justify-center items-center absolute left-0 top-0 right-0 bottom-0 full-width full-height">
             <h3 className="HomeBox__header full-width center body-serif absolute m0">{this.props.header}</h3>
           </div>
         </div>
