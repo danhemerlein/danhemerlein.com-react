c7557874c35b70a9ef52b195dd4f51aa4e351059
Dan <danhemerlein@gmail.com>
2018-12-01 20:12:28 -0500

    add resposive styles to contact

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -8,7 +8,7 @@ export default class Contact extends Component {
   render() {
     return (
       <div className="Contact body-serif my2 flex flex-column items-center justify-center">
-        <div className="flex flex-column items-center justify-center col-3">
+        <div className="flex flex-column items-center justify-center col-12">
           <h3 className="p0 m0">{this.props.cta}</h3>
           <h3 className="p0 mt0 mx0 mb2">{this.props.ctaTwo}</h3>
           <SignUpForm />
