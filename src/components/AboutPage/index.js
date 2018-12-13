cfae6cd09403bb8734bc8d026bab8dbf53e9b85d
Dan <danhemerlein@gmail.com>
2018-12-13 15:35:32 -0500

    add responsive styles to about page

--- src/components/AboutPage/index.js
+++ src/components/AboutPage/index.js
@@ -10,11 +10,13 @@ class AboutPage extends Component {
   render() {
     return (
       <div>
-        <div className="AboutPage my2 flex flex justify-center">
-          <div className="col-12 md-col-6">
-            <Image src={this.props.image.fields.file.url} alt={this.props.image.fields.title}/>
-          </div>
-          <div className="col-12 md-col-6">
+        <div className="AboutPage my2 flex flex flex-column justify-center">
+          <div className="col-12">
+            <div className="flex justify-center items-center">
+              <div className="col-12 md-col-6 lg-col-4">
+                <Image src={this.props.image.fields.file.url} alt={this.props.image.fields.title}/>
+              </div>
+            </div>
             <p className="m0 p4 body-serif">{this.props.text}</p>
           </div>
           <ScrollToTop scrollStepInPx="75" delayInMs="10" />
