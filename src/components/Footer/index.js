cc5495805e62f41ec05c52dbce0d92d26aa8b8ac
Dan <danhemerlein@gmail.com>
2020-04-26 19:38:37 -0400

    work on footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -6,43 +6,61 @@ export default class Footer extends Component {
   render() {
     return (
       <div className="Footer">
-        <div className="xs-hide flex justify-center items-center">
-          <div className="col-6">
-            <ul className="Footer__list list-style-none p0">
-              <li className="inline-block">
-                <a href="https://github.com/danhemerlein" target="_blank" rel="noopener noreferrer" titl="Github Icon">
-                  <span>github</span>
-                </a>
-              </li>
-              <li className="inline-block ml2">
-                <a href="https://www.linkedin.com/in/danhemerlein/" target="_blank" rel="noopener noreferrer" title="LinkedIn Icon">
-                  <span>linkedin</span>
-                </a>
-              </li>
-              <li className="inline-block ml2">
-                <a href="https://open.spotify.com/user/123676733/playlist/5apn8cxFTIuQfDiRSv8zdn?si=MLCfQOJTRhKaR7hem34gRg" target="_blank" rel="noopener noreferrer" title="Headphone Icon">
-                  <span>spotify</span>
-                </a>
-              </li>
-              <li className="inline-block ml2">
-                <a href="https://www.instagram.com/danhemerlein/" target="_blank" rel="noopener noreferrer" title="Instagram Icon">
-                  <span>instagram</span>
-                </a>
-              </li>
-            </ul>
-          </div>
-          <div className="col-6 flex justify-end">
+        <div className="flex justify-center items-center">
+          <ul className="Footer__list list-style-none p0">
+            <li className="inline-block">
+              <a
+                href="https://github.com/danhemerlein"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                github
+              </a>
+            </li>
+            <li className="inline-block ml2">
+              <a
+                href="https://workingnotworking.com/58170-dan"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                working not working
+              </a>
+            </li>
+            {/* <li className="inline-block ml2">
+              <a
+                href="https://www.linkedin.com/in/danhemerlein/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                linkedin
+              </a>
+            </li> */}
+            <li className="inline-block ml2">
+              <a
+                href="https://open.spotify.com/user/123676733/playlist/5apn8cxFTIuQfDiRSv8zdn?si=MLCfQOJTRhKaR7hem34gRg"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                spotify
+              </a>
+            </li>
+            <li className="inline-block ml2">
+              <a
+                href="https://www.instagram.com/danhemerlein/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                instagram
+              </a>
+            </li>
+          </ul>
+          <div className="Footer__small-container flex">
             <small className="Footer__small self-end">
               © Dan Hemerlein 2020
             </small>
           </div>
         </div>
-        <div className="sm-hide md-hide lg-hide full-width center">
-          <small className="Footer__small">
-            © Dan Hemerlein 2020
-          </small>
-        </div>
       </div>
-    )
+    );
   }
 }
--- src/components/MobileNav/index.js
+++ src/components/MobileNav/index.js
@@ -8,18 +8,16 @@ import './MobileNav.scss'
 export default class MobileNav extends Component {
   render() {
     return (
-      <div className="MobileNav bg-white flex flex-column">
+      <div className="MobileNav bg-white flex flex-column p1">
         <div className="flex items-end justify-end">
-          <div className="MobileNav__close-icon pointer pr1 pt1">
+          <div className="MobileNav__close-icon pointer">
             <CloseIcon clickHandler={this.props.clickHandler} />
           </div>
         </div>
         <nav role="navigation">
           <ul className="list-style-none p0 flex items-center justify-center flex-column">
             <li className="MobileNav__list-item body-serif my2">
-              <Link to="/code">
-                code
-              </Link>
+              <Link to="/code">code</Link>
             </li>
             <li className="MobileNav__list-item  body-serif my2">
               <Link to="/music">music</Link>
@@ -35,6 +33,58 @@ export default class MobileNav extends Component {
             </li>
             <li className="MobileNav__list-item  body-serif my2">
               <Link to="/">home</Link>
+            </li>
+
+            <hr className="MobileNav__hr"></hr>
+
+            <li className="MobileNav__list-item  body-serif my2">
+              <a
+                href="https://github.com/danhemerlein"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                github
+              </a>
+            </li>
+
+            <li className="MobileNav__list-item  body-serif my2">
+              <a
+                href="https://workingnotworking.com/58170-dan"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                working not working
+              </a>
+            </li>
+
+            {/* <li className="MobileNav__list-item  body-serif my2">
+              <a
+                href="https://www.linkedin.com/in/danhemerlein/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                linkedin
+              </a>
+            </li> */}
+
+            <li className="MobileNav__list-item  body-serif my2">
+              <a
+                href="https://open.spotify.com/user/123676733/playlist/5apn8cxFTIuQfDiRSv8zdn?si=MLCfQOJTRhKaR7hem34gRg"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                spotify
+              </a>
+            </li>
+
+            <li className="MobileNav__list-item  body-serif my2">
+              <a
+                href="https://www.instagram.com/danhemerlein/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                instagram
+              </a>
             </li>
           </ul>
         </nav>
