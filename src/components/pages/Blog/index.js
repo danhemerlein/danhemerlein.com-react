25673191649bed9c259247ba4d1d1b97144a9e44
Dan Hemerlein <danhemerlein@gmail.com>
2021-03-13 14:11:36 -0500

    add some data to the blog, lol

--- src/components/pages/Blog/index.js
+++ src/components/pages/Blog/index.js
@@ -1,11 +1,44 @@
-import React from 'react';
+import styled from 'styled-components'
+import React from 'react'
+import data from './q1-2021.json'
+
+const CategoryTitle = styled.strong`
+  font-weight: 900;
+`
+
+// const MonthTitle = styled(CategoryTitle)
 
 const Blog = () => {
-  return (
-    <div className="blog">
-      a small blog
-    </div>
-  );
-};
+  const movies = data[1]
 
-export default Blog;
+  return (
+    <section className='blog'>
+      <h1 className='mb_5 text-center'>music movies, books & tv shows</h1>
+      <h2 className='text-center'>january - march 2021</h2>
+
+      <div>
+        <CategoryTitle>movies</CategoryTitle>
+        {movies.movies.map((movie, key) => {
+          return (
+            <div key={key} className='my1'>
+              <p>
+                <CategoryTitle>{movie.month}</CategoryTitle>
+              </p>
+
+              {movie.data.map((data, key) => {
+                return (
+                  <p key={key}>
+                    <span>{data.title}</span>&nbsp;
+                    <span>({data.releaseYear})</span>
+                  </p>
+                )
+              })}
+            </div>
+          )
+        })}
+      </div>
+    </section>
+  )
+}
+
+export default Blog
