b516150cc66289fe417d6d37bd0e29c249ea3fca
Dan Hemerlein <danhemerlein@gmail.com>
2021-06-20 16:13:43 -0400

    fix makeKey bug in project link

--- README.md
+++ README.md
@@ -2,29 +2,27 @@
 
 ## Current Active Music Projects
 
-1. Notes from the Archive: Recordings 2011-2016 by Maggie Rogers
-2. i finally understand - young and nauseous remix by young and nauseous
-3. 400 Coffees by minikit
-4. CONVERSATIONS by Cannon
+1. mood ring / jillian by young and nauseous
+2. bulletproof limousine by young and nauseous
+3. Notes from the Archive: Recordings 2011-2016 by Maggie Rogers
+4. i finally understand - young and nauseous remix by young and nauseous
 
-5. BROKEN MIRRORS by Cannon
-6. Post Grad by Modern Diet
-7. Tarot Cards by Katie Glasgow
-8. Songwiring by Panther Hollow
+5. 400 Coffees by minikit
+6. CONVERSATIONS by Cannon
+7. BROKEN MIRRORS by Cannon
+8. In The Dark by minikit
 
-9. 400 Coffees - Ursae Flip by minikit, Ursae
-10. In The Dark by minikit
-11. Letter by Panther Hollow
-12. Midtown by Katie Glasgow
+9. Boys Travel Alone by Katie Glasgow
+10. Tarot Cards by Katie Glasgow
+11. Songwiring by Panther Hollow
+12. Red Eye by Modern Diet
 
-13. July 4 by Modern Diet
-14. 4am (Good for You) by minikit
-15. Jamie by Panther Hollow
-16. Red Eye by Modern Diet
+13. Letter by Panther Hollow
+14. Jamie by Panther Hollow
+15. Post Grad by Modern Diet
+16. July 4 by Modern Diet
 
-17. Sit Down and Dance by Modern Diet
-18. Atoms in the Universe by Panther Hollow
-19. Blue Jeep by Modern Diet
+
 
 ## Current Active Code Projects
 
--- src/components/pages/MusicProject/ProjectLink/index.js
+++ src/components/pages/MusicProject/ProjectLink/index.js
@@ -1,29 +1,54 @@
 import React from "react";
-import { makeKey } from "utils";
 import "./styles.scss";
 
 const Links = (props) => {
   const { key, link } = props;
   let hasLink = false;
-  const i = ["•", "•", "•", "•", "•", "•", "•", "•", "•", "•", "•", "•"];
-  const j = ["•", "•", "•", "•", "•", "•", "•", "•", "•", "•", "•", "•"];
+  const i = [
+    ["i0", "•"],
+    ["i1", "•"],
+    ["i2", "•"],
+    ["i3", "•"],
+    ["i4", "•"],
+    ["i5", "•"],
+    ["i6", "•"],
+    ["i7", "•"],
+    ["i8", "•"],
+    ["i9", "•"],
+    ["i10", "•"],
+    ["i11", "•"],
+  ];
+  const j = [
+    ["j0", "•"],
+    ["j1", "•"],
+    ["j2", "•"],
+    ["j3", "•"],
+    ["j4", "•"],
+    ["j5", "•"],
+    ["j6", "•"],
+    ["j7", "•"],
+    ["j8", "•"],
+    ["j9", "•"],
+    ["j10", "•"],
+    ["j11", "•"],
+  ];
   const k = [
-    ".",
-    ".",
-    ".",
-    ".",
-    ".",
-    ".",
-    ".",
-    ".",
-    ".",
-    ".",
-    ".",
-    ".",
-    ".",
-    ".",
-    ".",
-    ".",
+    ["k0", "."],
+    ["k1", "."],
+    ["k2", "."],
+    ["k3", "."],
+    ["k4", "."],
+    ["k5", "."],
+    ["k6", "."],
+    ["k7", "."],
+    ["k8", "."],
+    ["k9", "."],
+    ["k10", "."],
+    ["k11", "."],
+    ["k12", "."],
+    ["k13", "."],
+    ["k14", "."],
+    ["k15", "."],
   ];
   if (link.link !== undefined) {
     hasLink = true;
@@ -36,19 +61,19 @@ const Links = (props) => {
           href={link.link}
           target="_blank"
           rel="noopener noreferrer"
-          key={makeKey(key)}
         >
           <div
             className="ProjectLink__inner color-white
             flex justify-between w100 mb2"
-            key={makeKey(key)}
           >
-            {i.map((sym, iSymbolKey) => {
-              return <span key={makeKey(iSymbolKey)}>{sym}</span>;
+            {i.map((item) => {
+              const [id, sym] = item;
+              return <span key={id}>{sym}</span>;
             })}
             <span>{link.title}</span>
-            {j.map((sym, jSymbolKey) => {
-              return <span key={makeKey(jSymbolKey)}>{sym}</span>;
+            {j.map((item) => {
+              const [id, sym] = item;
+              return <span key={id}>{sym}</span>;
             })}
           </div>
         </a>
@@ -62,8 +87,10 @@ const Links = (props) => {
         >
           <div className="color-white flex justify-between w100 mb1">
             <span>{key}</span>
-            {k.map((sym, kSymbolKey) => {
-              return <span key={makeKey(kSymbolKey)}>{sym}</span>;
+            {k.map((item) => {
+              const [id, sym] = item;
+
+              return <span key={id}>{sym}</span>;
             })}
             <span>{link.title}</span>
           </div>
