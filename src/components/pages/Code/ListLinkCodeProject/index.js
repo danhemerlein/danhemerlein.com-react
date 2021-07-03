d0d4406e63b9858d7e743c343c6c0b8957b3f7c4
Dan Hemerlein <danhemerlein@gmail.com>
2021-07-03 13:35:30 -0400

    complete code page pre design

--- src/components/pages/Code/ListLinkCodeProject/index.js
+++ src/components/pages/Code/ListLinkCodeProject/index.js
@@ -1,22 +1,46 @@
 import React from "react";
-import "./styles.scss";
+import styled from "styled-components";
+import { FlexContainer, P } from "styles/elements";
+import { above } from "styles/utilities";
+import { remHelper } from "utils";
+
+const ListLink = styled.div`
+  width: 100%;
+  margin-bottom: ${remHelper[16]};
+  padding: 0 ${remHelper[16]};
+
+  ${above.tablet`
+    width: 50%;
+    padding: 0;
+  `}
+`;
+
+const Inner = styled(FlexContainer)`
+  ${above.tablet`
+    justify-content: center;
+  `}
+`;
+
+const StyledAnchor = styled.a`
+  text-decoration: underline;
+  text-align: center;
+  color: ${({ theme }) => theme.light.black};
+
+  &:visited {
+    color: ${({ theme }) => theme.light.black};
+  }
+`;
 
 const ListLinkCodeProject = ({ project }) => {
+  const { link, title } = project.fields;
   return (
-    <div className="ListLinkCodeProject col-12 md:col-6 mb2 px2 md:px0">
-      <div className="flex md:justify-center">
-        <a
-          href={project.fields.link}
-          target="_blank"
-          rel="noopener noreferrer"
-          className="ListLinkCodeProject__a text-center"
-        >
-          <h4 className="ListLinkCodeProject__headline m0">
-            {project.fields.title}
-          </h4>
-        </a>
-      </div>
-    </div>
+    <ListLink>
+      <Inner>
+        <StyledAnchor href={link} target="_blank" rel="noopener noreferrer">
+          <P>{title}</P>
+        </StyledAnchor>
+      </Inner>
+    </ListLink>
   );
 };
 
