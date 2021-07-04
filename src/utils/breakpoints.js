a96b381e1fe150f7e465bda14e229e2bc6f1e9c7
Dan Hemerlein <danhemerlein@gmail.com>
2021-07-04 16:08:04 -0400

    remove scss

--- src/utils/breakpoints.js
+++
@@ -1,8 +0,0 @@
-const breakpoints = {
-  'mobile': "(min-width: 320px)",
-  'tablet': "(min-width: 720px)",
-  'desktop': "(min-width: 1024px)",
-  'desktop-max': "(min-width: 1444px)"
-};
-
-export default breakpoints;
